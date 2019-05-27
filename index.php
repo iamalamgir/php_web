842639e8a5d7538f8382a75251bde9d7a2391b78
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-05-28 02:21:47 +0600

    add index file

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+    $fonts = "verdana";
+    $bgcolor = "orange";
+    $fontcolor = "blue";
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title>PHP Syntax</title>
+    <style>
+    .body{font-family:<?php echo $fonts;?>}
+
+    .phpcoding{width: 900px; margin: 0 auto; background:<?php echo"#ddd" ?>;}
+
+    .headeroption, .footeroption{background:<?php echo $bgcolor; ?>; color: <?php echo $fontcolor; ?>;
+     padding: 15px; text-align: center;}
+
+    .headeroption h2, .footeroption h2{margin: 0;}
+
+    .maincontent{min-height: 400px; padding: 20px;}
+    </style>
+</head>
+<body>
+
+<div class="phpcoding">
+    <section class="headeroption">
+        <h2><?php echo"PHP Fundamentals Training"; ?></h2>
+    </section>
+    <section class="maincontent">
+    1. String
+    2. Interger
+    3. Float
+    4. Boolean
+    5. Array
+    6. Object
+    7. NULL
+    8. Besource
+    <br/>
+    <br/>
+    7. NULL
+    <br/>
+        <?php
+            $x = "PHP is nice";
+            var_dump($x);
+        ?>
+    </section>
+    <section class="footeroption">
+        <h2><?php echo"www.trainingwithliveproject.com"; ?></h2>
+    </section>
+</div>
+
+</body>
+</html>
