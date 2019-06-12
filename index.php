7ce20deab6008a73d4c1ee1bdd67d84b85f6c2af
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-13 00:19:54 +0600

    php_file_open_read_close

--- index.php
+++ index.php
@@ -1,21 +1,61 @@
 <?php include 'header.php'; ?>
-        <section class="maincontent">
+    <section class="maincontent">
         
-            <br/>
-            <hr/>
-            PHP File Handling
-            <span style= "float:right">
-                <?php
-                    date_default_timezone_set('Asia/Dhaka');
-                    echo "Time : ".date("h:i:sa")."<br/>";
-                ?>
-            </span>
-            <hr/>
-            <br/>
-        
+        <br/>
+        <hr/>
+        PHP File Open/Read/Close
+        <span style= "float:right">
             <?php
-                echo readfile("text.txt");   
+                date_default_timezone_set('Asia/Dhaka');
+                echo "Time : ".date("h:i:sa")."<br/>";
             ?>
-            
-        </section>
+        </span>
+        <hr/>
+        <br/>
+
+        <?php
+            $ourfile = fopen("text.txt", "r") or die("File not found !!");
+            echo fread($ourfile,filesize("text.txt"));
+            fclose($ourfile);
+            echo "<br/>";
+            echo "<br/>";  
+        ?>
+
+        <?php
+            $ourfile = fopen("text.txt", "r") or die("File not found !!");
+            echo fgets($ourfile,filesize("text.txt"));
+            fclose($ourfile);
+            echo "<br/>";
+            echo "<br/>";   
+        ?>
+
+        <?php
+            $ourfile = fopen("text.txt", "r") or die("File not found !!");
+            echo fgetc($ourfile);
+            fclose($ourfile);
+            echo "<br/>";
+            echo "<br/>";   
+        ?>
+
+        <?php
+            $ourfile = fopen("text.txt", "r") or die("File not found !!");
+            while(!feof($ourfile)){
+                echo fgets($ourfile)."<br/>";
+            }
+            fclose($ourfile);
+            echo "<br/>";
+            echo "<br/>";   
+        ?>
+
+        <?php
+            $ourfile = fopen("text.txt", "r") or die("File not found !!");
+            while(!feof($ourfile)){
+                echo fgetc($ourfile)."<br/>";
+            }
+            fclose($ourfile);
+            echo "<br/>";
+            echo "<br/>";   
+        ?>
+        
+    </section>        
 <?php include 'footer.php'; ?>
