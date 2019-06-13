ecf17a331204c1455da4a6bb750a1c6f3bd14d73
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-13 22:37:20 +0600

    php_sessions

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
-<?php include 'header.php'; ?>
+<?php 
+session_start();
+include 'header.php'; 
+?>
     <section class="maincontent">
         
         <br/>
         <hr/>
-        PHP File Upload
+        PHP Sessions
         <span style= "float:right">
             <?php
                 date_default_timezone_set('Asia/Dhaka');
@@ -14,17 +17,14 @@
         <br/>
 
         <?php
-           if(isset($_FILES['image'])){
-                $filename = $_FILES['image']['name'];
-                $filetmp = $_FILES['image']['tmp_name'];
-                move_uploaded_file($filetmp, "images/".$filename);
-                echo "Image uploaded Successfully";
-           }
+           $_SESSION['user']     = " Alamgir";
+           $_SESSION['password'] = " 123";
+
+           echo "Username is".$_SESSION['user']."<br/>";
+           //session_unset();
+           echo "password is".$_SESSION['password'];
+           session_destroy();
         ?>
-        <form method= "POST" action= "" enctype= "multipart/form-data ">
-            <input type= "file" name= "image"/>
-            <input type= "submit" Value= "Submit"/>
-        </form>
 
     </section>        
 <?php include 'footer.php'; ?>
