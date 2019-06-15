8082704ed43ebe140c68c03d8f12cbe92332b26e
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-15 23:24:54 +0600

    create_array

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ setcookie('visited', "", time() - 3600);
         
         <br/>
         <hr/>
-        PHP require or require_once
+        PHP array() Function - Create Array
         <span style= "float:right">
             <?php
                 date_default_timezone_set('Asia/Dhaka');
@@ -19,16 +19,39 @@ setcookie('visited', "", time() - 3600);
         
             <?php
                 /*
-                 require "new.php";
-                 
-                require "new.php";
-                require "new.php";
+                $car = array("Volvo", "BMW", "Toyota");
+                echo $car[1];
 
-                require_once "new.php";
+                $car = array("Volvo", "BMW", "Toyota");
+                $length = count($car);
+                echo $length;
+
+                $car = array("Volvo", "BMW", "Toyota");
+                $length = count($car);
+                for($i = 0; $i < $length; $i++){
+                    echo $car[$i]."<br>";
+                }
+
+                $age = array(
+                    "Siam"    => "24",
+                    "Hasive"  => "26",
+                    "Alamgir" => "23",
+                    "Mahin"   => "30",
+                    "Imran"   => "27",
+                    "Belal"   => "28",
+                );
+                foreach($age as $key=> $value){
+                    echo "Name = ".$key.", Age = ".$value;
+                    echo "<br>";
+                }
                 */
 
-                require "new.php";
-                require_once "new.php";
+                $car = array(
+                    array("Volvo", "100", "90"),
+                    array("BMW", "50", "70"),
+                    array("Toyota", "120", "190")
+                );
+                echo $car[2][0];
                 
             ?>
             
