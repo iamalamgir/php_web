ed4a17681308839cfcc90fd48b9b97316c6aeff8
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-16 16:33:34 +0600

    array_pad

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ setcookie('visited', "", time() - 3600);
         
         <br/>
         <hr/>
-        PHP array() Function - array_multisort
+        PHP array() Function - array_pad
         <span style= "float:right">
             <?php
                 date_default_timezone_set('Asia/Dhaka');
@@ -19,48 +19,19 @@ setcookie('visited', "", time() - 3600);
         
             <?php
                 /*
-                $arr_one = array("Ox", "Dog", "Cat", "Horse", "Bear");
-                array_multisort($arr_one);
-                   
-                print("<pre>");
-                print_r($arr_one);
-                print("</pre>");
-
-                $arr_one = array("Ox", "Dog", "Cat", "Horse", "Bear");
-                $arr_two = array("Ant", "Tiger", "Lion", "Fish", "Monkey");
-                array_multisort($arr_one, $arr_two);
-                   
-                print("<pre>");
-                print_r($arr_one);
-                print("</pre>");
+               $arr = array("red", "green");
+                $result = array_pad($arr, 5, "blue");
 
                 print("<pre>");
-                print_r($arr_two);
-                print("</pre>");
-
-                $arr_one = array("Ox", "Dog", "Cat", "Horse", "Bear");
-                $arr_two = array("Ant", "Tiger", "Lion", "Fish", "Monkey");
-                array_multisort($arr_one, SORT_DESC, $arr_two, SORT_ASC);
-                   
-                print("<pre>");
-                print_r($arr_one);
-                print("</pre>");
-
-                print("<pre>");
-                print_r($arr_two);
+                print_r($result);
                 print("</pre>");
                 */
 
-                $arr_one = array("Ox", "Dog", "Cat", "Horse", "Bear");
-                $arr_two = array("Ant", "Tiger", "Lion", "Fish", "Monkey");
-                array_multisort($arr_one, SORT_ASC, $arr_two);
-                   
-                print("<pre>");
-                print_r($arr_one);
-                print("</pre>");
+                $arr = array("red", "green");
+                $result = array_pad($arr, -5, "blue");
 
                 print("<pre>");
-                print_r($arr_two);
+                print_r($result);
                 print("</pre>");
                 
             ?>
