a14954b1a455e8b66b4f3776cc8576380486e4af
Alamgir Hossain <mdalamgir.pro@gmail.com>
2019-06-17 23:41:27 +0600

    get_data_from_a_data_table_in_javascript

--- header.php
+++ header.php
@@ -23,6 +23,15 @@
     p{margin: 0;}
 
     input[type="text"]{width: 250px;}
+
+    select{font-size: 18px; padding: 2px 5px; width: 250px;}
+
+    .tblone{width: 420px; border: 2px solid #F3DFDF; margin: 20px 0;}
+    .tblone td{padding: 5px 10px;}
+    table.tblone tr:nth-child(2n+1){background: #fff; height: 30px;}
+    table.tblone tr:nth-child(2n){background: #f1f1f1; height: 30px;}
+
+    #myform{width: 400px; border:1px solid #fff; padding: 10px;}
     </style>
 </head>
 <body>
@@ -34,7 +43,7 @@
         <section class="maincontent">
         <br/>
         <hr/>
-        Get selected option value in JavaScript
+        Get data from a data table in JavaScript
         <span style= "float:right">
             <?php
                 date_default_timezone_set('Asia/Dhaka');
